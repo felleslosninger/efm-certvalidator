1ddca83b47db606d0b317760bfaceccfcbb9ba0e
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-11-09 21:28:46 +0100

    OCSPRule should not fail certificates without OCSP information.

--- src/main/java/no/difi/certvalidator/rule/OCSPRule.java
+++ src/main/java/no/difi/certvalidator/rule/OCSPRule.java
@@ -2,8 +2,9 @@ package no.difi.certvalidator.rule;
 
 import no.difi.certvalidator.api.CertificateBucket;
 import no.difi.certvalidator.api.CertificateValidationException;
-import no.difi.certvalidator.api.ValidatorRule;
 import no.difi.certvalidator.api.FailedValidationException;
+import no.difi.certvalidator.api.ValidatorRule;
+import org.bouncycastle.asn1.x509.Extension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.security.provider.certpath.OCSP;
@@ -31,6 +32,10 @@ public class OCSPRule implements ValidatorRule {
     @Override
     public void validate(X509Certificate certificate) throws CertificateValidationException {
         try {
+            // Certificates without OCSP information is not subject to OCSP validation.
+            if (certificate.getExtensionValue(Extension.authorityInfoAccess.getId()) == null)
+                return;
+
             X509Certificate issuer = intermediateCertificates.findBySubject(certificate.getIssuerX500Principal());
             if (issuer == null)
                 throw new FailedValidationException(String.format("Unable to find issuer certificate '%s'", certificate.getIssuerX500Principal().getName()));
---
+++ src/test/java/no/difi/certvalidator/rule/OcspRuleTest.java
@@ -0,0 +1,33 @@
+package no.difi.certvalidator.rule;
+
+import no.difi.certvalidator.Validator;
+import no.difi.certvalidator.api.CertificateBucket;
+import no.difi.certvalidator.api.CertificateValidationException;
+import no.difi.certvalidator.api.ValidatorRule;
+import no.difi.certvalidator.util.KeyStoreCertificateBucket;
+import no.difi.certvalidator.util.SimpleCertificateBucket;
+import org.testng.annotations.Test;
+
+import java.security.cert.X509Certificate;
+
+public class OCSPRuleTest {
+
+    /**
+     * OCSP should be tested only for certificates containing such information, just like CRL.
+     */
+    @Test
+    public void certificateWithoutOCSP() throws CertificateValidationException {
+        X509Certificate certificate = Validator.getCertificate(getClass().getResourceAsStream("/selfsigned.cer"));
+        ValidatorRule rule = new OCSPRule(new SimpleCertificateBucket(certificate));
+        rule.validate(certificate);
+    }
+
+    @Test
+    public void certificateWithOCSP() throws CertificateValidationException {
+        KeyStoreCertificateBucket keyStoreCertificateBucket = new KeyStoreCertificateBucket("JKS", getClass().getResourceAsStream("/peppol-test.jks"), "peppol");
+        ValidatorRule rule = new OCSPRule(keyStoreCertificateBucket.toSimple("peppol-ap", "peppol-smp"));
+        rule.validate(Validator.getCertificate(getClass().getResourceAsStream("/peppol-test-smp-difi.cer")));
+
+    }
+
+}
