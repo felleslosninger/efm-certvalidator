58a185267dfbabfc97e09b4f2bb05d065e7cf3a3
Thomas Johansen <Thomas.Johansen@difi.no>
2016-01-21 12:50:14 +0100

    Small refactoring for extensibility.

--- src/main/java/no/difi/certvalidator/util/CachingCrlFetcher.java
+++ src/main/java/no/difi/certvalidator/util/CachingCrlFetcher.java
@@ -12,9 +12,10 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509CRL;
 
 /**
- * Simple implementation of CRL updater. Used as default implementation.
+ * Default implementation of CRL fetcher, which caches downloaded CRLs. If a CRL is not cached, or the Next update-
+ * field of a cached CRL indicates there is an updated CRL available, an updated CRL will immediately be downloaded.
  */
-public class SimpleCrlFetcher implements CrlFetcher {
+public class CachingCrlFetcher implements CrlFetcher {
 
     private static final Logger logger = LoggerFactory.getLogger(CrlFetcher.class);
 
@@ -30,22 +31,32 @@ public class SimpleCrlFetcher implements CrlFetcher {
 
     private CrlCache crlCache;
 
-    public SimpleCrlFetcher(CrlCache crlCache) {
+    public CachingCrlFetcher(CrlCache crlCache) {
         this.crlCache = crlCache;
     }
 
     @Override
     public X509CRL get(String url) throws CertificateValidationException{
         X509CRL crl = crlCache.get(url);
-        if (crl == null || (crl.getNextUpdate() != null && crl.getNextUpdate().getTime() < System.currentTimeMillis())) {
-            crl = doUpdate(url);
+        if (crl == null) {
+            crl = notInCache(url);
+        } else if (crl.getNextUpdate() != null && crl.getNextUpdate().getTime() < System.currentTimeMillis()) {
+            crl = outdated(url, crl);
         } else if (crl.getNextUpdate() == null) {
             logger.warn("Next update not set for CRL with URL \"{}\"", url);
         }
         return crl;
     }
 
-    protected X509CRL doUpdate(String url) throws CertificateValidationException {
+    protected X509CRL notInCache(String url) throws CertificateValidationException {
+        return download(url);
+    }
+
+    protected X509CRL outdated(String url, X509CRL outdatedCrl) throws CertificateValidationException {
+        return download(url);
+    }
+
+    protected X509CRL download(String url) throws CertificateValidationException {
         logger.debug("Downloading CRL from {}...", url);
 
         try {
