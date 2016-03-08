d284afd9c0a3880d0598e029d7c4816906e78d77
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-03-08 22:08:02 +0100

    Adding KeyStoreCertificateBucket.startsWith(...).

--- src/main/java/no/difi/certvalidator/util/KeyStoreCertificateBucket.java
+++ src/main/java/no/difi/certvalidator/util/KeyStoreCertificateBucket.java
@@ -8,10 +8,7 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Reads a keystore from input stream and keeps it in memory.
@@ -109,6 +106,32 @@ public class KeyStoreCertificateBucket implements CertificateBucket {
     public SimpleCertificateBucket toSimple(String... aliases) throws CertificateBucketException {
         SimpleCertificateBucket certificates = new SimpleCertificateBucket();
         toSimple(certificates, aliases);
+        return certificates;
+    }
+
+    /**
+     * Adding certificates identified by prefix from keystore to a SimpleCertificateBucket.
+     */
+    public void startsWith(SimpleCertificateBucket certificates, String prefix) throws CertificateBucketException {
+        try {
+            KeyStore keyStore = getKeyStore();
+            Enumeration<String> aliasesEnumeration = keyStore.aliases();
+            while (aliasesEnumeration.hasMoreElements()) {
+                String alias = aliasesEnumeration.nextElement();
+                if (alias.startsWith(prefix))
+                    certificates.add((X509Certificate) keyStore.getCertificate(alias));
+            }
+        } catch (Exception e) {
+            throw new CertificateBucketException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Create a new SimpleCertificateBucket and adding certificates based on prefix.
+     */
+    public SimpleCertificateBucket startsWith(String prefix) throws CertificateBucketException {
+        SimpleCertificateBucket certificates = new SimpleCertificateBucket();
+        startsWith(certificates, prefix);
         return certificates;
     }
 
